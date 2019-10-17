41b7dd472af66bcbe66e91af65acf085bdcfca68
smart <smart@DESKTOP-USOSIEK>
2019-10-17 14:41:08 +0200

    17/7 Last Commit Work

--- Ecommerce/src/main/java/pages/Home.java
+++ Ecommerce/src/main/java/pages/Home.java
@@ -1,10 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Home extends PageBase {
-	public Home(WebDriver driver) {
+	
+	
+	public Home(WebDriver driver) 
+	{
 		super(driver);
 	}
-
+	//by pageFactory 
+		/*@FindBy(linkText="Register")
+		 WebElement registerLink;
+		 */
+	
+	
+	//method that click on link 
+	public void openRegistrationPage()
+	{
+		registerLink.click();
+	}
+	
 }
--- Ecommerce/src/main/java/pages/PageBase.java
+++ Ecommerce/src/main/java/pages/PageBase.java
@@ -1,11 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
 
 public class PageBase {
 	
 	static public WebDriver driver;
+	WebElement registerLink=driver.findElement(By.linkText("Register"));
 	
 	// Create Constructor paramaterized "take web driver as parameter"
 	public PageBase(WebDriver driver) {
